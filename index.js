5160b8b8f4bcdc21daaf36e06ada51031955d003
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-07-10 15:37:47 +0530

    resume backend addded

---
+++ index.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2');
+const express = require('express')
+const app = express ();
+const fs = require('fs');
+const path = require('path')
+const PORT = 5000;
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password12',
+  database: 'myresume'
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to the database');
+});
+
+app.get('/piyush/resume/overleaf', (req, res) => {
+  const filePath = path.join(__dirname, 'Resume', 'file.pdf');
+  res.sendFile(filePath, err => {
+    if (err) {
+      console.error('Error sending the file:', err);
+      res.status(500).send('Error sending the file');
+    }
+  });
+});
+
+app.listen(PORT , () => {
+  console.log(`App is running on port ${PORT}`)
+})
